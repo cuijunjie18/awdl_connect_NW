3a1ab7402a367c4cba7c9df0bf2ad1d1c26a1ca3
junjiecui <2871843852@qq.com>
2026-03-31 18:37:12 +0800

    Initial Commit

---
+++ awdl_NW/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  awdl_NW
+//
+//  Created by junjiecui on 2026/3/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ awdl_NW/awdl_NWApp.swift
@@ -0,0 +1,17 @@
+//
+//  awdl_NWApp.swift
+//  awdl_NW
+//
+//  Created by junjiecui on 2026/3/31.
+//
+
+import SwiftUI
+
+@main
+struct awdl_NWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
